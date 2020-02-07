26141ca80d9b6e014babb5d5167f4848d821e188
Bill Niblock <azulien@gmail.com>
2020-02-06 20:41:26 -0500

    Improve functionality

--- sidebar/sidedice.js
+++ sidebar/sidedice.js
@@ -1,21 +1,28 @@
-var buttons = document.querySelectorAll(".dr_die_button");
 var dice = document.querySelectorAll(".dr_die");
 
-function update_results(val, cnt) {
-  var res = document.querySelector("#dr_results")
-  var roll = Math.floor((Math.random() * Math.floor(val)) + 1)
+function update_results(cnt, val, mod, ech) {
+  let res = document.querySelector("#dr_results")
+  let roll = 0
 
-  res.innerHTML = roll
+  for (let i = 0; i < cnt; i++) {
+    roll += Math.floor((Math.random() * Math.floor(val)) + 1)
+    if (ech) { roll += +mod }
+  }
+
+  if (ech) {
+    res.innerHTML = roll
+  } else {
+    res.innerHTML = roll + +mod
+  }
 }
 
-buttons.forEach(function(b) {
-  b.addEventListener('click', function() {
-      update_results(b.value)
-  })
-})
-
 dice.forEach(function(d) {
-    d.children[0].addEventListener('click', function() {
-        update_results(d.children[0].value, d.children[1].value)
+    d.children[1].addEventListener('click', function() {
+        update_results(
+          d.children[0].value, 
+          d.children[1].value,
+          d.children[2].value,
+          d.children[3].checked
+        )
     })
 })
